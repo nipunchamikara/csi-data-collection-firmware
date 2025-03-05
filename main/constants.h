0b4499c42e7e76037252d1a208730027edbdf89f
Nipun Chamikara Weerasiri <nipunchamikara@yahoo.com>
2025-03-05 17:08:06 +0530

    Increase packet send frequency

--- main/constants.h
+++ main/constants.h
@@ -2,7 +2,7 @@
 #define CONSTANTS_H
 
 #define CONFIG_LESS_INTERFERENCE_CHANNEL 11
-#define CONFIG_SEND_FREQUENCY 20
+#define CONFIG_SEND_FREQUENCY 25
 #define TOTAL_DEVICES 3
 #define DEFAULT_DEVICE_ID 2
 #define TIMEOUT 10
@@ -11,5 +11,7 @@
 #define TAG "csi_send_recv"
 #define CSI_DATA_LENGTH 256
 #define CSI_DATA_ARR_LEN 4
+#define ORG_ID {0x18, 0xfe, 0x34}
+#define PAYLOAD_MAGIC_NUMBER {0x0F, 0xA7, 0x5A, 0x09}
 
 #endif // CONSTANTS_H
